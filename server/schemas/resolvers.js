1f0b45614af18a439c9870fa6887f544420d26e7
jdub <jessicaclaing@gmail.com>
2024-03-05 19:03:48 -0800

    added base User model. added mutations for login and addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-//TODO: add in models here once created
+const { User } = require('../models')
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
@@ -7,8 +7,28 @@ const resolvers = {
     },
     
     Mutation: {
-        //!!!!ADD LOGIN and token to use auth.js
-        //need to have a user model for this
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
     },
 };
 
